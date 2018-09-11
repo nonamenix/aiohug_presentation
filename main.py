09d3f13263c236d66d56f0bc5451751a720db3a1
d_ivanof <d_ivanof@wargaming.net>
2018-09-11 10:59:43 +0300

    Step 4 Cast types

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ def create_app():
     routes = aiohug.RouteTableDef()
 
     @routes.get("/hello/")
-    async def hello(name):
-        return web.Response(text=f"Hello, {name}")
+    async def hello(name, count=1):
+        return web.Response(text=f"Hello, {name}\n" * count)
 
     app = web.Application()
     app.add_routes(routes)
@@ -18,5 +18,5 @@ def create_app():
 async def test_app(test_client):
     app = create_app()
     client = await test_client(app)
-    resp = await client.get("/hello/", params={"name": "world"})
-    assert await resp.text() == "Hello, world"
+    resp = await client.get("/hello/", params={"name": "world", "count": 5})
+    assert await resp.text() == ("Hello, world\n" * 5)
