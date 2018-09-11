651a851c41e6ea3f4d8f9648b560a03179f5eb5b
d_ivanof <d_ivanof@wargaming.net>
2018-09-11 10:52:07 +0300

    Step 1 Test it

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from aiohttp import web
 
 
-async def hello(request):
-    # unnecessary `request` object
-    return web.Response(text="Hello, world")
+def create_app():
+    async def hello(request):
+        return web.Response(text="Hello, world")
+
+    app = web.Application()
+    app.add_routes([web.get("/", hello)])
+    return app
 
 
-app = web.Application()
-app.add_routes([web.get("/", hello)])
-web.run_app(app)
+async def test_app(test_client):
+    app = create_app()
+    client = await test_client(app)
+    resp = await client.get("/")
+    assert await resp.text() == "Hello, world"
 
-# Let's test it
+# I like register handlers with decorators
