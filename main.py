4a753fc6b9c6518ef13bb421e6a3d61701f02091
d_ivanof <d_ivanof@wargaming.net>
2018-09-11 10:58:47 +0300

    Step 3 Arg from query

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import aiohug
 from aiohttp import web
 
 
 def create_app():
-    routes = aiohug.RouteTableDef()
+    routes = web.RouteTableDef()
 
-    @routes.get("/hello/{name}")
-    async def hello(name):
+    @routes.get("/hello/")
+    async def hello(request: web.Request):
+        # needless go to doc
+        name = request.rel_url.query.get("name", "world")
         return web.Response(text=f"Hello, {name}")
 
     app = web.Application()
@@ -18,5 +19,5 @@ def create_app():
 async def test_app(test_client):
     app = create_app()
     client = await test_client(app)
-    resp = await client.get("/hello/world")
+    resp = await client.get("/hello/", params={"name": "world"})
     assert await resp.text() == "Hello, world"
