30f32bc77ef4d6d140f2e55f56549eb65eb6d253
d_ivanof <d_ivanof@wargaming.net>
2018-09-11 10:53:59 +0300

    Step 3 Arg from url

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from aiohttp import web
 def create_app():
     routes = web.RouteTableDef()
 
-    @routes.get("/")
-    async def hello(request):
-        return web.Response(text="Hello, world")
+    @routes.get("/hello/{name}")
+    async def hello(request: web.Request):
+        # needless go to doc
+        name = request.match_info['name']
+        return web.Response(text=f"Hello, {name}")
 
     app = web.Application()
     app.add_routes(routes)
@@ -17,5 +19,5 @@ def create_app():
 async def test_app(test_client):
     app = create_app()
     client = await test_client(app)
-    resp = await client.get("/")
+    resp = await client.get("/hello/world")
     assert await resp.text() == "Hello, world"
