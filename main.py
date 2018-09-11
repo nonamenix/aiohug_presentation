5627bb1a1c27fafeb6ee7976915c6b848d78d4e4
d_ivanof <d_ivanof@wargaming.net>
2018-09-11 10:52:35 +0300

    Step 2 With decorator

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from aiohttp import web
 
 
 def create_app():
+    routes = web.RouteTableDef()
+
+    @routes.get("/")
     async def hello(request):
         return web.Response(text="Hello, world")
 
     app = web.Application()
-    app.add_routes([web.get("/", hello)])
+    app.add_routes(routes)
+
     return app
 
 
@@ -15,5 +19,3 @@ async def test_app(test_client):
     client = await test_client(app)
     resp = await client.get("/")
     assert await resp.text() == "Hello, world"
-
-# I like register handlers with decorators
